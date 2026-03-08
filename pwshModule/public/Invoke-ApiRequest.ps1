847824a171584206e786f4d8e12f7c8b0c27f6ee
Brian Schmidt Pedersen <briped@users.noreply.github.com>
2026-03-08 23:30:46 +0100

    Token checking

--- pwshModule/public/Invoke-ApiRequest.ps1
+++ pwshModule/public/Invoke-ApiRequest.ps1
@@ -22,25 +22,38 @@ function Invoke-ApiRequest {
         [string]
         $ContentType = 'application/json'
     )
-    begin {
-        Write-Debug -Message "$($MyInvocation.MyCommand.Name): $($PSCmdlet.MyInvocation.BoundParameters | ConvertTo-Json -Compress)"
+    Write-Debug -Message "$($MyInvocation.MyCommand.Name): $($PSCmdlet.MyInvocation.BoundParameters | ConvertTo-Json -Compress -WarningAction SilentlyContinue)"
+
+    # Check for the configuration and its keys.
+    if (!$Script:Config -or !$Script:Config.Key -or !$Script:Config.KeyId -or 
+        !$Script:Config.ClientId -or !$Script:Config.TokenUrl -or !$Script:Config.TimeToLive) {
+        throw "Configuration is missing. Please create a configuration using New-ApiConfig or import one using Import-ApiConfig."
     }
-    process {
-        if (!$Script:access_token) {
-            $Response = Request-AccessToken
-            $Script:access_token = $Response.access_token
-        }
-        $Headers = @{
-            Authorization = "Bearer $($Script:access_token)"
-        }
-        $Attributes = @{
-            Headers = $Headers
-            Method = $Method
-            Uri = $Uri
-        }
-        $Response = Invoke-RestMethod @Attributes
-        $Response
+    $Attributes = @{
+        Key        = $Script:Config.Key
+        KeyId      = $Script:Config.KeyId
+        ClientId   = $Script:Config.ClientId
+        TokenUrl   = $Script:Config.TokenUrl
+        TimeToLive = $Script:Config.TimeToLive
     }
+    # Check for access token and its validity.
+    if (!$Script:Api -or !$Script:Api.AccessToken -or !$Script:Api.ExpiresAt -or 
+        $Script:Api.ExpiresAt -gt [datetime]::Now.AddSeconds(-30)) {
+        # Request a new access token.
+        $Response = Request-AccessToken @Attributes
+        $Script:Api.AccessToken = $Response.access_token
+        $Script:Api.ExpiresAt = [datetime]::Now.AddSeconds($Response.expires_in)
+    }
+    $Headers = @{
+        Authorization = "Bearer $($Script:Api.AccessToken)"
+    }
+    $Attributes = @{
+        Headers = $Headers
+        Method = $Method
+        Uri = $Uri
+    }
+    $Response = Invoke-RestMethod @Attributes
+    $Response
     <#
     .SYNOPSIS
     .DESCRIPTION
