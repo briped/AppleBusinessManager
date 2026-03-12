8a106b6592a9af5a10c2e3fc66b67864a85269b8
Brian Schmidt Pedersen <briped@users.noreply.github.com>
2026-03-12 08:00:36 +0100

    Merged

--- pwshModule/public/Get-Server.ps1
+++ pwshModule/public/Get-Server.ps1
@@ -1,37 +1,58 @@
 function Get-Server {
-    [CmdletBinding(DefaultParameterSetName = 'Limit')]
+    [CmdletBinding(DefaultParameterSetName = 'NoID')]
     param (
-        [Parameter()]
+        [Parameter(ParameterSetName = 'ID'
+                ,  Mandatory = $true
+                ,  ValueFromPipeline = $true
+                ,  ValueFromPipelineByPropertyName = $true)]
+        [Alias('Id')]
+        [string]
+        $ServerId
+        ,
+        [Parameter(ParameterSetName = 'NoID')]
         [ValidateSet('serverName', 'serverType', 'createdDateTime', 'updatedDateTime', 'devices')]
         [string[]]
         $Fields
         ,
-        [Parameter(ParameterSetName = 'Limit')]
+        [Parameter()]
         [ValidateRange(1, 1000)]
         [int]
         $Limit
         ,
-        [Parameter(ParameterSetName = 'All')]
+        [Parameter()]
         [Switch]
         $All
+        ,
+        [Parameter()]
+        [switch]
+        $Raw
     )
     begin {
-        Write-Debug -Message "$($MyInvocation.MyCommand.Name): $($PSCmdlet.MyInvocation.BoundParameters | ConvertTo-Json -Compress -WarningAction SilentlyContinue)"
+        Write-Debug -Message "$($MyInvocation.MyCommand.Name): ParameterSet: '$($PSCmdlet.ParameterSetName)'. $($PSCmdlet.MyInvocation.BoundParameters | ConvertTo-Json -Compress -WarningAction SilentlyContinue)"
     }
     process {
         $QueryString = [System.Web.HttpUtility]::ParseQueryString($null)
         $UriBuilder = [System.UriBuilder]::new($Script:Config.ApiUrl)
         $UriBuilder.Path += "/$([uri]::EscapeDataString('mdmServers'))"
-        if ($PSBoundParameters.ContainsKey('Limit')) { $QueryString.Set('limit', $Limit) }
-        if ($PSCmdlet.ParameterSetName -eq 'All') { $QueryString.Set('limit', 1000) }
+        if ($ServerId) {
+            $UriBuilder.Path += "/$([uri]::EscapeDataString($ServerId))"
+            $UriBuilder.Path += "/$([uri]::EscapeDataString('relationships'))"
+            $UriBuilder.Path += "/$([uri]::EscapeDataString('devices'))"
+        }
+        if ($Limit) { $QueryString.Set('limit', $Limit) }
+        if ($All) { $QueryString.Set('limit', 1000) }
         if ($PSBoundParameters.ContainsKey('Fields')) { $QueryString.Set('fields[orgDevices]', $Fields -join ',') }
         $UriBuilder.Query = $QueryString.ToString()
         $Uri = $UriBuilder.Uri
         do {
             $Response = Invoke-ApiRequest -Method Get -Uri $Uri
-            if ($PSCmdlet.ParameterSetName -eq 'Limit') { return $Response }
+            if (!$All) {
+                if ($Raw) { return $Response }
+                else { return $Response.data }
+            }
             $Uri = $Response.links.next
-            $Response.data
+            if ($Raw) { $Response }
+            else { $Response.data }
         } while ($Uri)
     }
     <#
@@ -51,5 +72,22 @@ limit
 integer
 The number of included related resources to return.
 Maximum: 1000
+
+https://developer.apple.com/documentation/applebusinessmanagerapi/get-all-device-ids-for-a-device-management-service
+Get the Device Serial Numbers for a Device Management Service
+Get a list of device serial numbers assigned to a device management service.
+Apple Business Manager API 1.5+
+URL
+GET https://api-business.apple.com/v1/mdmServers/{id}/relationships/devices
+Path Parameters
+id
+string
+(Required) The unique identifier for the resource.
+Query Parameters
+limit
+integer
+The number of included related resources to return.
+Maximum: 1000
+
 #>
 }
